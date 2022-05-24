8a43b1c360a74098492eaadd4765ff6ece23a27c
Bonghak Lee <bolee@c2r5s2.42seoul.kr>
2022-05-24 15:56:29 +0900

    basic usage of mysql module

---
+++ nodejs/mysql.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '12341234',
+	database: 'test'
+});
+
+connection.connect();
+
+connection.query('SELECT * FROM topic', (error, results, fields) => {
+	if (error) {
+		console.log(error);
+	}
+	console.log(results);
+});
+
+connection.end();
